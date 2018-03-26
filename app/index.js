550462cf47d27564a9a3c2a8b38baa66d07e9338
Gustavo Gutierrez <imguss@gmail.com>
2018-03-26 00:16:33 -0700

    changed name to AppComponent

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // const App = require('./components/app');
-import App from './components/app';
+import App from './components/AppComponent';
 
 ReactDOM.render(
   <App />,
