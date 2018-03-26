554186dda1f17a9851e44ed44ddc0b05a2edf673
Gustavo Gutierrez <imguss@gmail.com>
2018-03-26 00:19:29 -0700

    separated text and title to better work with grid

---
+++ app/components/DonkeyKongTextComponent.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+require('../style.css');
+
+export default class DonkeyKongText extends Component {
+  render() {
+    return (
+      // <div className="donkey-kong-grid">
+      //   <div className="donkey-kong-image">
+      //   </div>
+
+      //   <h2 className="donkey-kong-title">Donkey Kong</h2>
+      // </div>
+      // <h2 className="donkey-kong-title"Donkey Kong></h2>
+
+      <div className="donkey-kong-text">
+        <p>
+          A Donkey Kong game with a twist! In this Donkey Kong replica I used the basics of web development to create a
+        </p>
+      </div>
+    );
+  }
+}
---
+++ app/components/DonkeyKongTitleComponent.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+require('../style.css');
+
+export default class DonkeyKongTitle extends Component {
+  render() {
+    return (
+    //   <div className="donkey-kong-text">
+    //     <p>
+    //       A Donkey Kong game with a twist! In this Donkey Kong replica I used the basics of web development to create a
+    //     </p>
+    // </div>
+
+    <h2 className="donkey-kong-title">Donkey Kong</h2>
+    );
+  }
+}
